87df1f70a2b0b7a92ba4613d24f413d1c5f7a36e
TringuG <richardnalepka@gmail.com>
2022-03-27 13:14:33 +0000

    Some changes

--- pages/index.vue
+++ pages/index.vue
@@ -78,34 +78,6 @@
             </svg>
 
             <svg
-              @click="goTo(messengerUrl)"
-              class="w-5 h-5 text-gray-400 transition duration-300 cursor-pointer  dark:text-gray-600 hover:text-green-400 dark:hover:text-green-400"
-              fill="currentColor"
-              role="img"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-            >
-              <title>Messenger icon</title>
-              <path
-                d="M.001 11.639C.001 4.949 5.241 0 12.001 0S24 4.95 24 11.639c0 6.689-5.24 11.638-12 11.638-1.21 0-2.38-.16-3.47-.46a.96.96 0 00-.64.05l-2.39 1.05a.96.96 0 01-1.35-.85l-.07-2.14a.97.97 0 00-.32-.68A11.39 11.389 0 01.002 11.64zm8.32-2.19l-3.52 5.6c-.35.53.32 1.139.82.75l3.79-2.87c.26-.2.6-.2.87 0l2.8 2.1c.84.63 2.04.4 2.6-.48l3.52-5.6c.35-.53-.32-1.13-.82-.75l-3.79 2.87c-.25.2-.6.2-.86 0l-2.8-2.1a1.8 1.8 0 00-2.61.48z"
-              />
-            </svg>
-
-            <svg
-              @click="goTo(whatsappUrl)"
-              class="w-5 h-5 text-gray-400 transition duration-300 cursor-pointer  dark:text-gray-600 hover:text-green-400 dark:hover:text-green-400"
-              fill="currentColor"
-              role="img"
-              viewBox="0 0 24 24"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <title>WhatsApp icon</title>
-              <path
-                d="M17.472 14.382c-.297-.149-1.758-.867-2.03-.967-.273-.099-.471-.148-.67.15-.197.297-.767.966-.94 1.164-.173.199-.347.223-.644.075-.297-.15-1.255-.463-2.39-1.475-.883-.788-1.48-1.761-1.653-2.059-.173-.297-.018-.458.13-.606.134-.133.298-.347.446-.52.149-.174.198-.298.298-.497.099-.198.05-.371-.025-.52-.075-.149-.669-1.612-.916-2.207-.242-.579-.487-.5-.669-.51-.173-.008-.371-.01-.57-.01-.198 0-.52.074-.792.372-.272.297-1.04 1.016-1.04 2.479 0 1.462 1.065 2.875 1.213 3.074.149.198 2.096 3.2 5.077 4.487.709.306 1.262.489 1.694.625.712.227 1.36.195 1.871.118.571-.085 1.758-.719 2.006-1.413.248-.694.248-1.289.173-1.413-.074-.124-.272-.198-.57-.347m-5.421 7.403h-.004a9.87 9.87 0 01-5.031-1.378l-.361-.214-3.741.982.998-3.648-.235-.374a9.86 9.86 0 01-1.51-5.26c.001-5.45 4.436-9.884 9.888-9.884 2.64 0 5.122 1.03 6.988 2.898a9.825 9.825 0 012.893 6.994c-.003 5.45-4.437 9.884-9.885 9.884m8.413-18.297A11.815 11.815 0 0012.05 0C5.495 0 .16 5.335.157 11.892c0 2.096.547 4.142 1.588 5.945L.057 24l6.305-1.654a11.882 11.882 0 005.683 1.448h.005c6.554 0 11.89-5.335 11.893-11.893a11.821 11.821 0 00-3.48-8.413Z"
-              />
-            </svg>
-
-            <svg
               @click="goTo(discordUrl)"
               class="w-5 h-5 text-gray-400 transition duration-300 cursor-pointer  dark:text-gray-600 hover:text-green-400 dark:hover:text-green-400"
               fill="currentColor"
@@ -472,17 +444,17 @@
             :isOpened="currentlyOpened === 'projects'"
             v-on:activate="onOpenSection($event)"
             id="projects"
-            colorParent="from-teal-300 to-teal-500"
-            colorParent2="bg-teal-300"
-            colorNameBg="bg-teal-200"
-            colorNameText="text-teal-600"
-            colorNameDark="text-teal-500 dark:text-teal-400"
+            colorParent="from-red-300 to-red-500"
+            colorParent2="bg-red-300"
+            colorNameBg="bg-red-200"
+            colorNameText="text-red-600"
+            colorNameDark="text-red-500 dark:text-red-400"
             title="Successful Projects"
             iconD="M17 8h2a2 2 0 012 2v6a2 2 0 01-2 2h-2v4l-4-4H9a1.994 1.994 0 01-1.414-.586m0 0L11 14h4a2 2 0 002-2V6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2v4l.586-.586z"
           >
             <h1 class="text-xl font-bold text-black dark:text-gray-50">
               With more than
-              <span class="text-teal-400">20 finished projects</span> over one
+              <span class="text-red-400">20 finished projects</span> over one
               year of frelanding, I am a strong developer ready to bring my
               skill set into every project.
             </h1>
@@ -492,7 +464,7 @@
                 v-for="(project, i) of projects"
                 :key="i"
                 v-bind:class="project.grid"
-                class="relative p-2 bg-teal-400 rounded-lg cursor-pointer  group h-80"
+                class="relative p-2 bg-red-400 rounded-lg cursor-pointer  group h-80"
                 @click="onOpenGallery(i)"
               >
                 <div
@@ -677,16 +649,16 @@ export default Vue.extend({
       isDark: true,
 
       formSpreeKey: 'xrgjpobb',
-      fbUrl: 'https://www.facebook.com/matejbaco2000',
+      fbUrl: 'https://www.facebook.com/profile.php?id=100006881294301',
       messengerUrl: 'https://m.me/102386341636067',
-      gitHubUrl: 'https://github.com/meldiron',
-      discordUrl: 'https://discordapp.com/channels/@me/287294735054274560/',
+      gitHubUrl: 'https://github.com/tringug',
+      discordUrl: 'https://discordapp.com/channels/@me/239373923953672192/',
       whatsappUrl: 'https://wa.me/421919178798',
-      igUrl: 'https://www.instagram.com/matejbaco2000/',
+      igUrl: 'https://www.instagram.com/cj.risko6g/',
 
-      contactEmail: 'matejbaco2000@gmail.com',
-      constactTel: '+421 919 178 798',
-      contactTelPlain: '+421919178798',
+      contactEmail: 'richardnalepka@gmail.com',
+      constactTel: '+421 948 387 906',
+      contactTelPlain: '+421948387906',
 
       codingSkills: [
         {
@@ -1078,8 +1050,8 @@ export default Vue.extend({
     },
     onDownloadCV() {
       const link = document.createElement('a')
-      link.download = 'CV_Matej_Baco.pdf'
-      link.href = '/CV_Matej_Baco.pdf'
+      link.download = 'CV_Richard_Nalepka.pdf'
+      link.href = '/CV_Richard_Nalepka.pdf'
 
       document.body.appendChild(link)
       link.click()
@@ -1109,7 +1081,7 @@ export default Vue.extend({
         }, 300)
       }
 
-      this.iframe.src = '/CV_Matej_Baco.pdf'
+      this.iframe.src = '/CV_Richard_Nalepka.pdf'
     },
 
     goTo(url: string) {
